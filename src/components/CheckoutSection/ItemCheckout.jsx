f97c614a5b9e5d30412d405ea309514cef3a94b8
dheeraj_700 <dheeraj700agrahari@gmail.com>
2023-11-25 11:45:26 +0530

    checkout_page

--- src/components/CheckoutSection/ItemCheckout.jsx
+++ src/components/CheckoutSection/ItemCheckout.jsx
@@ -1,47 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { FaArrowLeftLong } from "react-icons/fa6";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useCart } from '../../CartContext/CartContext';
 
 const CheckoutPage = () => {
+  const { state } = useCart();
   const [sameAsShipping, setSameAsShipping] = useState(false);
   const [isPaymentSuccessful, setIsPaymentSuccessful] = useState(false);
   const [formData, setFormData] = useState({
-    email: '',
-    phoneNumber: '',
-    shippingAddress: '',
-    city: '',
-    state: '',
-    zipCode: '',
+    email: "",
+    phoneNumber: "",
+    shippingAddress: "",
+    city: "",
+    state: "",
+    zipCode: "",
     sameAsShipping: false,
-    billingAddress: '',
-    billingCity: '',
-    billingZipCode: '',
-    billingCountry: '',
-    cardholderName: '',
-    cardNumber: '',
-    expiryDate: '',
+    billingAddress: "",
+    billingCity: "",
+    billingZipCode: "",
+    billingCountry: "",
+    cardholderName: "",
+    cardNumber: "",
+    expiryDate: "",
   });
 
+   // Calculate total amount
+   const totalAmount = state.items.reduce((total, item) => total + item.price * item.quantity, 0);
 
   const handleInputChange = (e) => {
     const { name, value, type, checked } = e.target;
     setFormData((prevData) => ({
       ...prevData,
-      [name]: type === 'checkbox' ? checked : value,
+      [name]: type === "checkbox" ? checked : value,
     }));
   };
-
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // Check for required fields
-    const requiredFields = Object.keys(formData).filter((key) => key !== 'sameAsShipping');
+    const requiredFields = Object.keys(formData).filter(
+      (key) => key !== "sameAsShipping"
+    );
 
     if (!sameAsShipping) {
       const isFormValid = requiredFields.every((field) => formData[field]);
       if (!isFormValid) {
-        alert('Please fill in all required fields.');
+        alert("Please fill in all required fields.");
         return;
       }
     }
@@ -55,10 +60,10 @@ const CheckoutPage = () => {
   };
 
   return (
-    <div className="container mx-auto my-8" style={{ width: '80%' }}>
-        <Link to={`/cart`}>
+    <div className="container mx-auto my-8" style={{ width: "80%" }}>
+      <Link to={`/cart`}>
         <FaArrowLeftLong className="absolute top-12 left-20 text-2xl" />
-        </Link>
+      </Link>
       <div className="flex flex-wrap">
         {/* Left Side - Account Information, Shipping Address, Billing Address */}
         <div className="w-full lg:w-1/2 pr-4 mb-8">
@@ -79,7 +84,10 @@ const CheckoutPage = () => {
             />
           </div>
           <div className={`mb-4 pb-2 border-b `}>
-            <label htmlFor="phoneNumber" className="block text-sm text-gray-300">
+            <label
+              htmlFor="phoneNumber"
+              className="block text-sm text-gray-300"
+            >
               Phone Number:
             </label>
             <input
@@ -96,7 +104,10 @@ const CheckoutPage = () => {
 
           <h2 className="text-2xl font-semibold mb-4 mt-6">Shipping Address</h2>
           <div className={`mb-4 pb-2 border-b `}>
-            <label htmlFor="shippingAddress" className="block text-sm text-gray-300">
+            <label
+              htmlFor="shippingAddress"
+              className="block text-sm text-gray-300"
+            >
               Address:
             </label>
             <input
@@ -174,7 +185,10 @@ const CheckoutPage = () => {
             <>
               <h2 className="text-2xl font-semibold mb-4">Billing Address</h2>
               <div className={`mb-4 pb-2 border-b `}>
-                <label htmlFor="billingAddress" className="block text-sm text-gray-300">
+                <label
+                  htmlFor="billingAddress"
+                  className="block text-sm text-gray-300"
+                >
                   Address:
                 </label>
                 <input
@@ -188,7 +202,10 @@ const CheckoutPage = () => {
                 />
               </div>
               <div className={`mb-4 pb-2 border-b `}>
-                <label htmlFor="billingCity" className="block text-sm text-gray-300">
+                <label
+                  htmlFor="billingCity"
+                  className="block text-sm text-gray-300"
+                >
                   City:
                 </label>
                 <input
@@ -202,7 +219,10 @@ const CheckoutPage = () => {
                 />
               </div>
               <div className={`mb-4 pb-2 border-b `}>
-                <label htmlFor="billingZipCode" className="block text-sm text-gray-300">
+                <label
+                  htmlFor="billingZipCode"
+                  className="block text-sm text-gray-300"
+                >
                   ZIP Code:
                 </label>
                 <input
@@ -216,7 +236,10 @@ const CheckoutPage = () => {
                 />
               </div>
               <div className={`mb-4 pb-2 border-b `}>
-                <label htmlFor="billingCountry" className="block text-sm text-gray-300">
+                <label
+                  htmlFor="billingCountry"
+                  className="block text-sm text-gray-300"
+                >
                   Country:
                 </label>
                 <input
@@ -238,7 +261,10 @@ const CheckoutPage = () => {
           <h2 className="text-2xl font-semibold mb-4">Payment Details</h2>
           <form onSubmit={handleSubmit}>
             <div className={`mb-4 pb-2 border-b `}>
-              <label htmlFor="cardholderName" className="block text-sm text-gray-300">
+              <label
+                htmlFor="cardholderName"
+                className="block text-sm text-gray-300"
+              >
                 Cardholder Name:
               </label>
               <input
@@ -253,7 +279,10 @@ const CheckoutPage = () => {
               />
             </div>
             <div className={`mb-4 pb-2 border-b `}>
-              <label htmlFor="cardNumber" className="block text-sm text-gray-300">
+              <label
+                htmlFor="cardNumber"
+                className="block text-sm text-gray-300"
+              >
                 Card Number:
               </label>
               <input
@@ -268,7 +297,10 @@ const CheckoutPage = () => {
               />
             </div>
             <div className={`mb-4 pb-2 border-b `}>
-              <label htmlFor="expiryDate" className="block text-sm text-gray-300">
+              <label
+                htmlFor="expiryDate"
+                className="block text-sm text-gray-300"
+              >
                 Expiry Date:
               </label>
               <input
@@ -285,21 +317,24 @@ const CheckoutPage = () => {
 
             {/* Order Summary */}
             <h2 className="text-2xl font-semibold mb-4 mt-8">Order Summary</h2>
-            <div className="mb-4 pb-2">
-              <p className="text-sm text-gray-300">Item 1: $20.00</p>
-            </div>
-            <div className="mb-4 pb-2">
-              <p className="text-sm text-gray-300">Item 2: $30.00</p>
-            </div>
-
+            {state.items.map((item, index) => (
+              <div key={index} className="mb-4 pb-2">
+                <p className="text-sm text-gray-700">{`${item.title}: $${(
+                  item.price * item.quantity
+                ).toFixed(2)}`}</p>
+              </div>
+            ))}
             {/* Total Price */}
             <div className="mt-8">
-              <p className="text-xl font-semibold text-gray-300">Total Amount: $50.00</p>
+              <p className="text-xl font-semibold text-gray-700">{`Total Amount: $${totalAmount}`}</p>
             </div>
 
             {/* Make Payment Button */}
             <div className="mt-8">
-              <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+              <button
+                type="submit"
+                className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+              >
                 Make Payment
               </button>
             </div>
@@ -311,8 +346,13 @@ const CheckoutPage = () => {
       {isPaymentSuccessful && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white p-8 rounded shadow">
-            <p className="text-2xl font-semibold mb-4">Thanks for your payment!</p>
-            <button onClick={closePopup} className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+            <p className="text-2xl font-semibold mb-4">
+              Thanks for your payment!
+            </p>
+            <button
+              onClick={closePopup}
+              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+            >
               Close
             </button>
           </div>
